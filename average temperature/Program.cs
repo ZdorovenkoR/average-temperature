6a6b35a9c00f0a583098b43d17bd9b1f11992ce9
ZdorovenkoRoman <ZdorovenkoR@gmail.com>
2020-12-12 11:54:43 +0600

    Добавьте файлы проекта.

---
+++ average temperature/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace average_temperature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите максимальную температуру за сегодня");
+            int maxT = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("введите минимальную температуру за сегодня");
+            int minT = Convert.ToInt32(Console.ReadLine());
+            var aveT = (maxT + minT)/2;
+            Console.WriteLine($"средняя температура сегодня {aveT}");
+        }
+    }
+}
